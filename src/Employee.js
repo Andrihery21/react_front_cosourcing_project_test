e52d30986f2835fca7e91ce11ce7251f24fb0752
Andrihery <roadsterandry@gmail.com>
2023-03-25 15:23:39 +0300

    Tous les form in good place

--- src/Employee.js
+++ src/Employee.js
@@ -31,16 +31,165 @@ export class Employee extends Component{
     componentDidMount(){
         this.refreshList();
     }
+    changeNom = (e) => {
+      this.setState({ nom: e.target.value });
+    };
+    changePrenom = (e) => {
+      this.setState({ prenom: e.target.value });
+    };
+    changeEmail = (e) => {
+      this.setState({ email: e.target.value });
+    };
+    changePoste = (e) => {
+      this.setState({ poste: e.target.value });
+    };
+    changeSalaire = (e) => {
+      this.setState({ salaire: e.target.value });
+    };
+    changeDateEmbauche = (e) => {
+      this.setState({ dateEmbauche: e.target.value });
+    };
+    changeAvantage = (e) => {
+      this.setState({ avantage: e.target.value });
+    };
+
+    addClick() {
+      this.setState({
+        modalTitle: "Ajout employé",
+        employeeId: 0,
+        nom: "",
+        prenom: "",
+        email: "",
+        poste: "",
+        salaire: "",
+        dateEmbauche: "",
+        avantage: "",
+      });
+    }
+    editClick(emp) {
+      this.setState({
+        modalTitle: "Edit Employee",
+        employeeId: emp.employeeId,
+        nom: emp.nom,
+        prenom: emp.prenom,
+        email: emp.email,
+        poste: emp.poste,
+        salaire: emp.salaire,
+        dateEmbauche: emp.dateEmbauche,
+        avantage: emp.avantage
+      });
+    }
+  
+    createClick() {
+      fetch(variables.API_URL + "employee", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nom: this.state.nom,
+          prenom: this.state.prenom,
+          email: this.state.email,
+          poste: this.state.poste,
+          salaire: this.state.salaire,
+          dateEmbauche: this.state.dateEmbauche,
+          avantage: this.state.avantage,
+        }),
+      })
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            alert(result);
+            this.refreshList();
+          },
+          (error) => {
+            alert("Failed");
+          }
+        );
+    }
+
+    updateClick() {
+      fetch(variables.API_URL + "employee/", {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          employeeId: this.state.employeeId,
+          nom: this.state.nom,
+          prenom: this.state.prenom,
+          email: this.state.email,
+          poste: this.state.poste,
+          salaire: this.state.salaire,
+          dateEmbauche: this.state.dateEmbauche,
+          avantage: this.state.avantage,
+        }),
+      })
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            alert(result);
+            this.refreshList();
+          },
+          (error) => {
+            alert("Failed");
+          }
+        );
+    }
+  
+    deleteClick(id) {
+      if (window.confirm("Are you sure?")) {
+        fetch(variables.API_URL + "employee/" + id, {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              alert(result);
+              this.refreshList();
+            },
+            (error) => {
+              alert("Failed");
+            }
+          );
+      }
+    }
+
     
     render(){
              
         const{
             employees,
+            modalTitle,
+            employeeId,
+            nom,
+            prenom,
+            email,
+            poste,
+            salaire,
+            dateEmbauche,
+            avantage
             
         }=this.state;
 
         return(
             <div>
+               <button
+          type="button"
+          className="btn btn-primary m-2 float-end"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          onClick={() => this.addClick()}
+        >
+          Ajouter un employé
+        </button>
+
                    <table className="table table-hover table-striped">
           <thead>
             <tr>
@@ -111,7 +260,127 @@ export class Employee extends Component{
               </tr>
             ))}
           </tbody>
-        </table>     
+        </table> 
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-lg modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{modalTitle}</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="d-flex flex-row bd-highlight mb-3">
+     
+                        <div className="p-2 w-50 bd-highlight">
+
+             
+               
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Nom</span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={nom}
+                        onChange={this.changeNom}
+                      />
+                    </div>
+
+                  
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Prénom</span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={prenom}
+                        onChange={this.changePrenom}
+                      />
+                    </div>
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Email</span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={email}
+                        onChange={this.changeEmail}
+                      />
+                    </div>
+                  
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Poste</span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={poste}
+                        onChange={this.changePoste}
+                      />
+                    </div>
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Salaire</span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={salaire}
+                        onChange={this.changeSalaire}
+                      />
+                    </div>
+                   
+              
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Date d'embauche</span>
+                      <input
+                        type="date"
+                        className="form-control"
+                        value={dateEmbauche}
+                        onChange={this.changeDateEmbauche}
+                      />
+                    </div>
+                  
+                    <div className="input-group mb-3">
+                      <span className="input-group-text">Avantage</span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={avantage}
+                        onChange={this.changeAvantage}
+                      />
+                    </div> 
+                  
+                </div>
+
+                {employeeId === 0 ? (
+                  <button
+                    type="button"
+                    className="btn btn-primary float-start"
+                    onClick={() => this.createClick()}
+                  >
+                    Créer
+                  </button>
+                ) : null}
+
+                {employeeId !== 0 ? (
+                  <button
+                    type="button"
+                    className="btn btn-primary float-start"
+                    onClick={() => this.updateClick()}
+                  >
+                    Update
+                  </button>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div> 
+            </div>
             </div>
         )
     }
